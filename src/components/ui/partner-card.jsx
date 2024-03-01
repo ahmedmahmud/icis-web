eaa35d000642b2e8d0a86de836c5a9b8a2e739a1
ahmedmahmud <ahmedmahmud12971@gmail.com>
2024-03-01 20:13:22 +0000

    fix: key & nested p tags in partnercard

--- src/components/ui/partner-card.jsx
+++ src/components/ui/partner-card.jsx
@@ -15,15 +15,15 @@ export default function PartnerCard({ sponsor }) {
       <CardContent className="flex flex-col md:flex-row items-start md:items-center gap-4 md:gap-8">
         <div className="space-y-2 text-left">
           <h4 className="text-xl font-bold">{sponsor.name}</h4>
-          <p className="text-sm text-gray-500 dark:text-gray-400">
+          <div className="text-sm text-gray-500 dark:text-gray-400">
             {sponsor.description.map((description) => (
               <p key={description}>{description}</p>
             ))}
-          </p>
+          </div>
           <div className="gap-2 flex pt-4">
             {sponsor.buttons ? (
               sponsor.buttons.map(({ text, link }) => (
-                <a href={link}>
+                <a href={link} key={link}>
                   <Button variant="secondary" size="sm">
                     {text}
                   </Button>
